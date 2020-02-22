08944edb378b81db789dd43f7be3ef3b531053ec
makutak <takkouno@gmail.com>
2020-02-23 03:37:10 +0900

    array queueのコンストラクタ

--- src/array_queue.rs
+++ src/array_queue.rs
@@ -1,1 +1,12 @@
-pub struct ArrayQueue {}
+pub struct ArrayQueue {
+    pub a: Vec<u32>,
+    pub j: usize,
+    pub n: usize,
+}
+
+impl ArrayQueue {
+    pub fn new() -> ArrayQueue {
+        let ary = Vec::new();
+        ArrayQueue { a: ary, j: 0, n: 0 }
+    }
+}
--- tests/array_queue_test.rs
+++ tests/array_queue_test.rs
@@ -1,6 +1,21 @@
-use ods_rust::array_queue::*;
+#[cfg(test)]
+mod new_method_tests {
+    use ods_rust::array_queue::*;
+    #[test]
+    fn a_is_empty_vector() {
+        let aq = ArrayQueue::new();
+        assert!(aq.a.is_empty());
+    }
 
-#[test]
-fn hello_array_queue() {
-    println!("hello array queue!!");
+    #[test]
+    fn j_is_zero() {
+        let aq = ArrayQueue::new();
+        assert_eq!(aq.j, 0);
+    }
+
+    #[test]
+    fn n_is_zero() {
+        let aq = ArrayQueue::new();
+        assert_eq!(aq.n, 0);
+    }
 }
