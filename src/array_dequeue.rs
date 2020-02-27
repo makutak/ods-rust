4c70f9bd937ec790419e0aaae07087774e406ba7
makutak <takkouno@gmail.com>
2020-02-27 23:24:21 +0900

    get関数とそのテストの追加。落ちる

--- src/array_dequeue.rs
+++ src/array_dequeue.rs
@@ -10,4 +10,10 @@ impl ArrayDequeue {
         let ary = Vec::new();
         Self { a: ary, j: 0, n: 0 }
     }
+
+    pub fn get(&mut self, i: usize) -> u32 {
+        self.a[(self.j + i) % self.a.len()]
+    }
+
+    pub fn add(&mut self, x: u32) {}
 }
