655b1df9493f29b29cc451d6afdd6d53d444b7ac
makutak <takkouno@gmail.com>
2020-02-27 23:24:44 +0900

    漏れ

--- tests/array_dequeue.rs
+++ tests/array_dequeue.rs
@@ -3,19 +3,31 @@ mod new_method_tests {
     use ods_rust::array_dequeue::*;
     #[test]
     fn a_is_empty_vector() {
-        let aq = ArrayDequeue::new();
-        assert!(aq.a.is_empty());
+        let ad = ArrayDequeue::new();
+        assert!(ad.a.is_empty());
     }
 
     #[test]
     fn j_is_zero() {
-        let aq = ArrayDequeue::new();
-        assert_eq!(aq.j, 0);
+        let ad = ArrayDequeue::new();
+        assert_eq!(ad.j, 0);
     }
 
     #[test]
     fn n_is_zero() {
-        let aq = ArrayDequeue::new();
-        assert_eq!(aq.n, 0);
+        let ad = ArrayDequeue::new();
+        assert_eq!(ad.n, 0);
+    }
+}
+
+#[cfg(test)]
+mod get_method_tests {
+    use ods_rust::array_dequeue::*;
+    #[test]
+    fn get_indexed_value() {
+        let mut ad = ArrayDequeue::new();
+        ad.add(1);
+        ad.add(2);
+        assert_eq!(ad.get(0), 1);
     }
 }
