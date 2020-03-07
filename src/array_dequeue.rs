9b770601df23c3e2fa7bfa52bab28327ac4626e0
makutak <takkouno@gmail.com>
2020-03-07 23:31:01 +0900

    remove 実装

--- src/array_dequeue.rs
+++ src/array_dequeue.rs
@@ -45,7 +45,28 @@ impl ArrayDequeue {
         self.n += 1;
     }
 
-    pub fn remove(&mut self, i: usize) {}
+    pub fn remove(&mut self, i: usize) -> u32 {
+        let current_len = self.a.len();
+        let x = self.a[(self.j + i) % self.a.len()];
+
+        if i < self.n / 2 {
+            for k in (0..i).rev() {
+                self.a[(self.j + k) % current_len] = self.a[(self.j + k - 1) % current_len];
+            }
+            self.j = (self.j + 1) % current_len;
+        } else {
+            for k in i..(self.n - 1) {
+                self.a[(self.j + k) % current_len] = self.a[(self.j + k + 1) % current_len]
+            }
+        }
+
+        self.n -= 1;
+        if current_len >= 3 * self.n {
+            self.resize();
+        }
+
+        x
+    }
 
     pub fn resize(&mut self) {
         let mut b = vec![0; max(1, self.n * 2)];
