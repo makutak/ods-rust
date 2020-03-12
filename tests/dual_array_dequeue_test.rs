9c37f7b281c4c1e9fbd8ed518ffbac4f7d373ef7
makutak <takkouno@gmail.com>
2020-03-12 20:56:08 +0900

    修正

--- tests/dual_array_dequeue_test.rs
+++ tests/dual_array_dequeue_test.rs
@@ -2,7 +2,7 @@
 mod new_method_tests {
     use ods_rust::dual_array_dequeue::*;
     #[test]
-    fn front_back_are_empty() {
+    fn front_and_back_are_empty() {
         let daq = DualArrayDequeue::new();
         assert!(daq.front.is_empty());
         assert!(daq.back.is_empty());
