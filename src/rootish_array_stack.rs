db86e0c00cd86a54269d1bfd9d2e65e9dab09443
makutak <takkouno@gmail.com>
2020-04-18 02:18:47 +0900

    コンストラクタ定義

--- src/rootish_array_stack.rs
+++ src/rootish_array_stack.rs
@@ -4,3 +4,14 @@ pub struct RootishArrayStack {
     pub blocks: ArrayStack,
     pub n: usize,
 }
+
+impl RootishArrayStack {
+    pub fn new() -> Self {
+        let blocks = ArrayStack::new();
+        let n = 0;
+        Self {
+            blocks: blocks,
+            n: n,
+        }
+    }
+}
