6062ffd97bdc76889f45af61e65a1a29f655a815
makutak <takkouno@gmail.com>
2020-04-28 02:47:51 +0900

    計算順序間違ってた

--- src/rootish_array_stack.rs
+++ src/rootish_array_stack.rs
@@ -18,7 +18,7 @@ impl RootishArrayStack {
     }
 
     pub fn i2b(i: usize) -> i64 {
-        round::ceil(-3.0 + ((9 + 8 * i) as f64).sqrt() / 2.0, 3) as i64
+        round::ceil((-3.0 + ((9 + 8 * i) as f64).sqrt()) / 2.0, 0) as i64
     }
 
     pub fn get(&mut self, i: usize) {}
